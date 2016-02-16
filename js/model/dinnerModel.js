37f17608f7fc235da2f20a3601563e6e0e6bc553
Yuanlai <869494563@qq.com>
2016-02-16 15:33:36 +0100

    first version of Model

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -1,77 +1,96 @@
 //DinnerModel Object constructor
 var DinnerModel = function() {
- 
+
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
-	// var numberOfGuests;
-	var menus = new Array;
-	menus.push ("new dish");
+	var menu = [];
 
-	// using HashTable to contain those ingredients, with name being the key and detailed
-	// ingredients information stored in independent HashTables.
-	// var total_ingredients = {};
 
+  var numberOfGuests = 0;
 
 
 	this.setNumberOfGuests = function(num) {
 		//TODO Lab 2
-		numberOfGuests = num;
+    if(num > 0) {
+      numberOfGuests = num;
+    }
+
 	}
 
-	// should return 
+	// should return
 	this.getNumberOfGuests = function() {
 		//TODO Lab 2
-		// return $(numberOfGuests);
-		return 4;
+		return numberOfGuests;
 	}
 
-	//Returns the dish that is on the menu for selected type 
+	//Returns the dish that is on the menu for selected type
 	this.getSelectedDish = function(type) {
 		//TODO Lab 2
+    var selectDishMenu = [];
+    for(key in menu){
+      if(dishes[menu[key]].type == type){
+        selectDishMenu.push(this.getDish(menu[key]));
+      }
+    }
+    return selectDishMenu;
 	}
 
 	//Returns all the dishes on the menu.
 	this.getFullMenu = function() {
 		//TODO Lab 2
-		return menus;
+    var menuDishes = [];
+    for(key in menu){
+      menuDishes.push(this.getDish(menu[key]));
+    }
+    return menuDishes;
 	}
 
 	//Returns all ingredients for all the dishes on the menu.
 	this.getAllIngredients = function() {
 		//TODO Lab 2
+    var allIngredients = [];
+    for(key in menu){
+      allIngredients = allIngredients.concat(this.getDish(menu[key]).ingredients);
+    }
+    return allIngredients;
 	}
 
 	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
 	this.getTotalMenuPrice = function() {
 		//TODO Lab 2
-		var TotalMenuPrice = 0;
-		for(key in menus){
-			for(key_ingredient in menus[key]){
-				TotalMenuPrice += key_ingredient.price;
-			}
+		var totalMenuPrice = 0;
+		for(key in menu){
+			totalMenuPrice += this.getDish(menu[key]).price;
 		}
+    return totalMenuPrice;
 	}
 
 	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
 	//it is removed from the menu and the new one added.
 	this.addDishToMenu = function(id) {
-		//TODO Lab 2 
-		menus.push(getDish(id));
+		//TODO Lab 2
+    for(key in menu){
+      if(menu[key] == id){
+        return;
+      }
+    }
+		for(key in dishes){
+      if(dishes[key].id == id){
+        menu.push(id);
+      }
+    }
 	}
 
 	//Removes dish from menu
 	this.removeDishFromMenu = function(id) {
 		//TODO Lab 2
-		var found = false;
-		var index;
-		for(key in menus){
-			if(menus[key].id==id) {
-				index = key;
-				// got the item, and then return the index for deleting
-			}
-		}
-		menus.splice(index, 1);
-		window.alert("item deleted successfully!")
+	  for(key in menu){
+      if(menu[key] == id){
+        menu.splice(key,1);
+        window.alert("item deleted successfully!")
+        break;
+      }
+    }
 	}
 
 	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
@@ -80,7 +99,7 @@ var DinnerModel = function() {
 	this.getAllDishes = function (type,filter) {
 	  return $(dishes).filter(function(index,dish) {
 		var found = true;
-		// if any valid filter parameter is passed, the following operation would be processed. 
+		// if any valid filter parameter is passed, the following operation would be processed.
 		if(filter){
 			found = false;
 			$.each(dish.ingredients,function(index,ingredient) {
@@ -95,7 +114,7 @@ var DinnerModel = function() {
 			}
 		}
 	  	return dish.type == type && found;
-	  });	
+	  });
 	}
 
 	//function that returns a dish of specific ID
@@ -108,11 +127,11 @@ var DinnerModel = function() {
 	}
 
 
-	// the dishes variable contains an array of all the 
+	// the dishes variable contains an array of all the
 	// dishes in the database. each dish has id, name, type,
 	// image (name of the image file), description and
-	// array of ingredients. Each ingredient has name, 
-	// quantity (a number), price (a number) and unit (string 
+	// array of ingredients. Each ingredient has name,
+	// quantity (a number), price (a number) and unit (string
 	// defining the unit i.e. "g", "slices", "ml". Unit
 	// can sometimes be empty like in the example of eggs where
 	// you just say "5 eggs" and not "5 pieces of eggs" or anything else.
@@ -122,7 +141,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'toast.jpg',
 		'description':"In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'eggs',
 			'quantity':0.5,
 			'unit':'',
@@ -154,7 +173,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'sourdough.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'active dry yeast',
 			'quantity':0.5,
 			'unit':'g',
@@ -176,7 +195,7 @@ var DinnerModel = function() {
 		'type':'starter',
 		'image':'bakedbrie.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'round Brie cheese',
 			'quantity':10,
 			'unit':'g',
@@ -198,7 +217,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'extra lean ground beef',
 			'quantity':115,
 			'unit':'g',
@@ -260,7 +279,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'bakedbrie.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':1,
 			'unit':'pieces',
@@ -282,7 +301,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':2,
 			'unit':'pieces',
@@ -304,7 +323,7 @@ var DinnerModel = function() {
 		'type':'main dish',
 		'image':'meatballs.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ingredient 1',
 			'quantity':1,
 			'unit':'pieces',
@@ -326,7 +345,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
@@ -338,7 +357,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
@@ -350,7 +369,7 @@ var DinnerModel = function() {
 		'type':'dessert',
 		'image':'icecream.jpg',
 		'description':"Here is how you make it... Lore ipsum...",
-		'ingredients':[{ 
+		'ingredients':[{
 			'name':'ice cream',
 			'quantity':100,
 			'unit':'ml',
