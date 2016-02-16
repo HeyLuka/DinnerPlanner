631e3bb9d4d41b3d6643604e4b35592682e7dcff
lyl <869494563@qq.com>
2016-02-16 23:30:05 +0100

    confirmDinner-first

--- js/view/confirmDinner.js
+++ js/view/confirmDinner.js
@@ -10,4 +10,31 @@ var ConfirmDinner = function(container) {
 	this.selectDish_panel.hide();
 	this.dishInfo_panel.hide();
 	this.printoutMenu_page.hide();
+
+	this.myMenu_detail = container.find("#myMenu-detail");
+
+	for(key in model.dishes) {
+		var dish_instance = document.createElement('div');
+		$(dish_instance).attr({"class": "col-xs-4", "id": "dish-example"});
+		$(dish_instance).attr({"style": "height: 250px; display: block; margin-top: 20px; text-align: center"});
+		var dish_instance_img = document.createElement('img');
+		$(dish_instance_img).attr({"src": "images/"+model.getDish(model.dishes[key].id)["image"]});
+		$(dish_instance_img).attr({"class": "img-circle", "alt": "Dish-instance", "width": "100", "height": "100"});
+		var dish_instance_name = document.createElement('h3');
+		$(dish_instance_name).text(model.getDish(model.dishes[key].id)["name"]);
+		var dish_instance_desc = document.createElement('p');
+		$(dish_instance_desc).attr("style", "text-align: left")
+		// $(dish_instance_desc).text(model.getDish(model.dishes[key].id)["description"]);
+		var temp_string = model.getDish(model.dishes[key].id)["description"];
+		if(temp_string.length > 120){
+			temp_string = temp_string.substring(0,120)+"...";
+		}
+		$(dish_instance_desc).text(temp_string);
+
+		this.myMenu_detail.append(dish_instance);
+		$(dish_instance).append(dish_instance_img);
+		$(dish_instance).append(dish_instance_name);
+		$(dish_instance).append(dish_instance_desc);
+
+	}
 }
