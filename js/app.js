f1680cc9edcbc6291ce318d87eb490f9840474bc
HeyLuka <heyhippo2013@gmail.com>
2016-02-17 01:00:08 +0100

    pages not done.

--- js/app.js
+++ js/app.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
 	// exampleView = new ExampleView($("#exampleView"));
 
 	navigationBar = new NavigationBar($("#navigationBar"));
-	// startWizard = new StartWizard($("#displayField"));
-	myDinnerBar = new MyDinnerBar($("#displayField"));
+	startWizard = new StartWizard($("#displayField"));
+	// myDinnerBar = new MyDinnerBar($("#displayField"));
 	// detailedDish = new DetailedDish($("#displayField"));
 	// confirmDinner = new ConfirmDinner($("#displayField"));
 	// printRecipe = new PrintRecipe($("#displayField"));
